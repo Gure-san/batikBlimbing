6745f1a02b6ec78b327ce708e3bb8f7220eedf31
Gure-san <azrarafani@gmail.com>
2023-07-24 09:34:42 +0700

    chore: integrasi dengan shadcn

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    './public/**/*.astro',
-    'node_modules/preline/dist/*.js',
-  ],
+    './src/**/*.{ts,tsx,astro}',
+	],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [
-    require('preline/plugin'),
-    require('@tailwindcss/aspect-ratio')
-  ],
-};
+  plugins: [require("tailwindcss-animate")],
+}
